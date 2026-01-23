e7816deaf93fd2753976df6f4793f1309216eb4d
Samuel <samolayemi3@gmail.com>
2026-01-23 15:39:59 +0100

    Removed pinguard from index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,6 @@ import RecentActivityList from "@/components/home/RecentActivityList";
 import SuccessBanner from "@/components/home/SuccessBanner";
 import { NetworkError } from "@/components/NetworkError";
 import { OfflineBanner } from "@/components/OfflineBanner";
-import { PinGuard } from "@/components/PinGuard";
 import SpendingInsights from "@/components/SpendingInsights";
 import { useNetworkStatus } from "@/hooks/useNetworkStatus";
 import { useWallet } from "@/hooks/useWallet";
@@ -168,7 +167,6 @@ export default function Index() {
 
   return (
     <>
-    <PinGuard>
       <LinearGradient
         colors={[theme.colors.gradientStart, theme.colors.gradientEnd]}
         style={styles.container}
@@ -226,7 +224,6 @@ export default function Index() {
           />
         </ScrollView>
       </LinearGradient>
-      </PinGuard>
     </>
   );
 }
